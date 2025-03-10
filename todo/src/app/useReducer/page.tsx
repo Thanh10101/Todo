9b5ee9a82a4ffad435e18a02c98b74b576bf4ad2
Nguyen Thanh <trinhthanh20022@gmail.com>
2025-03-10 16:35:26 +0700

    useReducer

--- todo/src/app/useReducer/page.tsx
+++ todo/src/app/useReducer/page.tsx
@@ -1,28 +1,85 @@
 "use client"
-import React, { useRef } from 'react'
+import React, { useReducer } from 'react'
+
+const initValue = {
+  isLoading: false as boolean,
+  data: [] as IUser[],
+  error: null as string | null,
+}
+interface IUser {
+  first_name: string,
+  last_name: string,
+  email?: string,
+  id: number
+}
+type DataAction =
+  | { type: "FETCH_START" }
+  | { type: "FETCH_SUCCESS"; payload: IUser[] }
+  | { type: "FETCH_ERROR"; payload: string | null }
+
+const dataReducer = (state: typeof initValue, action: DataAction) => {
+  switch (action.type) {
+    case "FETCH_START":
+      return { ...state, isLoading: true, error: null }; // Reset error khi bắt đầu fetch
+    case "FETCH_SUCCESS":
+      return { ...state, isLoading: false, data: action.payload, error: null };
+    case "FETCH_ERROR":
+      return { ...state, isLoading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 
-export default function Page() {
-    type Account ={
-        username: string;
-        password :string;
+
+export default function UseReducer() {
+  const [count, dispatch] = useReducer((state, action) => {
+    switch (action) {
+      case "TANG":
+        return state + 1;
+      case "GIAM":
+        return state - 1;
+      default: return state;
     }
-    const ref = useRef<Account>({
-        username:"",
-        password:""
-    })
-    const handleClick =(e:React.ChangeEvent<HTMLInputElement>)=>{
-        const {name,value} = e.target
-        ref.current = { ...ref.current, [name]: value };
-    console.log(ref.current.username, ref.current.password);
-  
-    }
+  }, 0);
+  const [data, dataDispatch] = useReducer(dataReducer, initValue);
+
+  const getData = () => {
+    dataDispatch({ type: "FETCH_START" })
+    setTimeout(() => {
+      fetch("https://reqres.in/api/users")
+        .then((res) => res.json())
+        .then((res) => {
+          dataDispatch({ type: "FETCH_SUCCESS", payload: res.data })
+          console.log(res.data)
+        })
+        .catch((error) => {
+          dataDispatch({ type: "FETCH_ERROR", payload: error })
+
+        })
+    }, 2000)
+  }
   return (
     <>
-    <div>{JSON.stringify(ref)}</div>
-    <input name="username" type="text" onChange={handleClick}/>username
-    <input name="password" type="text" onChange={handleClick}/>password
-  
+      <p>Get API</p>
+      <button type="button" onClick={getData}>Get Data</button>
+      {data.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <p>{JSON.stringify(data.data)}</p>
+          <ul>
+            {data.data.map((user: IUser) => (
+              <li key={user.id}>{user.first_name} {user.last_name}</li>
+            ))}
+          </ul>
+        </>
+      )}
+
+      <p>Count {count}</p>
+      <button type="button" onClick={() => dispatch("TANG")}>Tang</button>
+      <button type="button" onClick={() => dispatch("GIAM")}>Giam</button>
     </>
   )
 }
+
--- todo/src/app/useState/page.tsx
+++ todo/src/app/useState/page.tsx
@@ -8,20 +8,20 @@ type Account = {
 
 export default function Home() {
   const [user, setUser] = useState<Account>({
-   username:"123",
-   password:""
+    username: "123",
+    password: ""
   })
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name,value} = e.target;
-   setUser(prev=>({...prev, [name]: value}));
-};
+    const { name, value } = e.target;
+    setUser(prev => ({ ...prev, [name]: value }));
+  };
   return (
-    <> <div> 
+    <> <div>
       <div>UseState</div>
       <div>{JSON.stringify(user)}</div>
-      <input type="username" name="username"   placeholder={user.username} onChange={handleChange}/>username
-       <input type="password" name = "password"   placeholder={user.password} onChange={handleChange}/>password
+      <input type="username" name="username" placeholder={user.username} onChange={handleChange} />username
+      <input type="password" name="password" placeholder={user.password} onChange={handleChange} />password
       {/* <button onClick={handDel}>Click</button> */}
     </div>
     </>
